9d8e56a14a6cc138516682a228ea2022f8e020c5
NYAMGEREL <powerlight_6127@yahoo.com>
2015-07-19 20:20:18 +0900

    new studying

---
+++ jjj.py
@@ -0,0 +1,9 @@
+current_year=2015
+born_year=1994
+age=current_year-born_year
+enter_year=2012
+grade=current_year-enter_year
+print(age)
+print("my age is %d MY grades is %d" % (age, grade))
+string_to_print=" my age is %d MY grade is %d" %(age,grade)
+print(string_to_print)
